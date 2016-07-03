ef2866e7b8a946511ab826d2a214dfdeb4504674
Kimserey <kimserey.lam@housing-insight.co.uk>
2016-07-03 19:11:09 +0100

    added files

--- BespokeJS.SiteletTest/Main.fs
+++ BespokeJS.SiteletTest/Main.fs
@@ -2,15 +2,49 @@ namespace BespokeJS.SiteletTest
 
 open WebSharper.Html.Server
 open WebSharper
+open WebSharper.UI.Next
+open WebSharper.UI.Next.Html
 open WebSharper.Sitelets
 
+
+[<JavaScript>]
+module Domain =
+    type Test = {
+        Greeting: string
+        Hello: Hello
+    }
+    and Hello =
+        | Hello
+        | Hi
+        | Hey
+
+[<JavaScript>]
+module Client =
+    open Domain
+    open WebSharper.UI.Next.Client
+
+    let page hello =
+        match hello.Hello with
+        | Hello -> text ("Hello, " + hello.Greeting)
+        | Hi -> text ("Hi, " + hello.Greeting)
+        | Hey -> text ("Hey, " + hello.Greeting)
+
 module Site =
-
-
+    open Domain
+    open WebSharper.UI.Next.Server
+    
+    type Index = Templating.Template<"Main.html">
+    
     let Main =
+        let hello = {
+            Greeting = "TEST TEST TEST"
+            Hello = Hey
+        }
+
         Sitelet.Sum 
             [
-                Sitelet.Content "" "" (fun _ -> Content.Text "hello world")
+                Sitelet.Content "" "" (fun _ -> 
+                    Index.Doc("Hello world", [ client <@ Client.page hello @> ]) |> Content.Page)
                 Sitelet.Content "test" "test" (fun _ -> Content.Text "test")
                 Sitelet.Content "more" "more" (fun _ -> Content.Text "more")
             ]
@@ -24,6 +58,7 @@ module SelfHostedServer =
     open Microsoft.Owin.FileSystems
     open WebSharper.Owin
 
+
     [<EntryPoint>]
     let Main args =
         let rootDirectory, url =
@@ -32,12 +67,15 @@ module SelfHostedServer =
             | [| url |] -> "..", url
             | [| |] -> "..", "http://localhost:9000/"
             | _ -> eprintfn "Usage: BespokeJS.SiteletTest ROOT_DIRECTORY URL"; exit 1
+    
+        let options = 
+            new WebSharperOptions<string>(
+                Debug = true, 
+                ServerRootDirectory = rootDirectory)
+    
         use server = WebApp.Start(url, fun appB ->
-            appB.UseStaticFiles(
-                    StaticFileOptions(
-                        FileSystem = PhysicalFileSystem(rootDirectory)))
-                .UseSitelet(rootDirectory, Site.Main)
-            |> ignore)
+            appB.UseStaticFiles(StaticFileOptions(FileSystem = PhysicalFileSystem(rootDirectory)))
+                .UseWebSharper(options.WithSitelet(Site.Main)) |> ignore)
         stdout.WriteLine("Serving {0}", url)
         stdin.ReadLine() |> ignore
         0
